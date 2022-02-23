35e5877a8e873bae273556fe2021235bd57c372b
TodoshiIdzuno <sheshin.alexanderkpc2052020@gmail.com>
2022-02-23 22:36:29 +0300

    Получение прав доступа

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import platform
+from pydrive.drive import GoogleDrive
+from pydrive.auth import GoogleAuth
 
+
+scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive',
+         'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive.appdata']
 
 def get_id():
-    return platform.node()
+    return get_access(platform.node())
+
+def get_access(device_id):
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
+    return drive, device_id
 
