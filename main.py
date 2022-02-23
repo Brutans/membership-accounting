4cfcd0519cdd9364967af4087f663b2e8606d50e
Brutans <mrbrutans@gmail.com>
2022-02-23 23:55:32 +0300

    Создание папки и таблиц

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import kivy
-kivy.require('1.0.7')
+def create_folder_and_sheet(drive):
+    folder_name = 'Учет абонементов'
+    folder = drive.CreateFile({'title': folder_name, 'mimeType': 'application/vnd.google-apps.folder'})
+    folder.Upload()
 
-from kivy.animation import Animation
-from kivy.app import App
-from kivy.uix.button import Button
+    folder_id = folder.get('id')
+    sheet_create = drive.CreateFile({'parents': [{'id': folder_id}], 'title': device_id,
+                                     'mimeType': 'application/vnd.google-apps.spreadsheet'})
 
-
-class TestApp(App):
-    pass
-
-
-if __name__ == '__main__':
-    Tpass
+    sheet_create.Upload()
+    return sheet_create
